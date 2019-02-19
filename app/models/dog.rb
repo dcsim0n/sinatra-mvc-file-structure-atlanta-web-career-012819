b565df8597ac6f55b39d867d17ae5e342c6fc9e1
Dana Simmons <dana@simmons-audio.com>
2019-02-19 13:32:33 -0500

    got tests passing, all done

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   attr_accessor :name, :age, :breed
   @@all = []
-  def initialize(name: nil, age: nil ,breed: nil)
+  def initialize(name, breed, age)
     @name = name
     @age = age
     @breed = breed
